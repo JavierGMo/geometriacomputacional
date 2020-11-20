08ec50db3f4dc589a7b40d1f66757e6a7e302fc9
JavierGMo <jinjuriki62@gmail.com>
2020-11-20 15:07:06 -0600

    TEST nodes and branchs

--- practica1.py
+++ practica1.py
@@ -2,36 +2,31 @@ import networkx as nx
 import graphviz
 
 G = nx.Graph()
-#Añadir nodos
-G.add_node("Kevin Bacon")
-G.add_node("Tom Hanks")
-G.add_nodes_from(["Meg Ryan", "Parker Posey", "Lisa Kudrow"])
- 
-#Añadir aristas
-G.add_edge("Kevin Bacon", "Tom Hanks")
-G.add_edge("Kevin Bacon", "Meg Ryan")
-G.add_edges_from([("Tom Hanks", "Meg Ryan"), ("Tom Hanks", "Parker Posey")])
-G.add_edges_from([("Parker Posey", "Meg Ryan"), ("Parker Posey", "Lisa Kudrow")])
 
-print(len(G.nodes))
-print(len(G.edges))
-print(G.nodes)
-print(G.edges)
+# Agregamos los nodos; n nodos
 
+G.add_node('V')
+G.add_node('R')
+G.add_node('A')
+G.add_node('AM')
 
-# asignar atributos a nodos y aristas
-G.nodes["Tom Hanks"]["oscars"] = 2
-G.edges["Kevin Bacon", "Tom Hanks"]["pelicula"] = "Apolo 13"
-G.edges["Kevin Bacon", "Meg Ryan"]["pelicula"] = "En carne viva"
-G.edges["Parker Posey", "Meg Ryan"]["pelicula"] = "Algo para recordar"
-G.edges["Parker Posey", "Tom Hanks"]["pelicula"] = "Tienes un email"
-G.edges["Parker Posey", "Lisa Kudrow"]["pelicula"] = "Esperando la hora"
+#agregamos las ramas; a que nodo va el nodo
+
+G.add_edge('V', 'R')
+G.add_edges_from([('A', 'AM'), ('R', 'AM')])
+
+#Agregamos los atributos a nodos y las aristas
+
+G.nodes['V']['colorNode'] = 'verde'
+
+# G.edges['V', 'R']['colorN'] = 'rama1'
+
 print(G.nodes(data=True))
-print(G.edges(data=True))
-print(G["Kevin Bacon"])
+print(G.nodes['V'])
 
+#Generamos la salida
 A = nx.nx_agraph.to_agraph(G)
 A.layout('dot')
-A.draw('salida.png') # guardar como png
- 
-# graphviz.Source(A.to_string())  mostrar en jupyter
+# A.draw('salida2.png')
+
+# print(G.nodes)
